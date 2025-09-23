d0cc5e0fcf2036d38197524c3962e5dc17403b12
Shin Changyoung <toyoaki800@gmail.com>
2025-09-23 18:59:18 +0900

    0923

--- iot9/src/Main.java
+++ iot9/src/Main.java
@@ -1,6 +1,3 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
 import util.HttpSendData;
 
 import java.io.IOException;
@@ -8,12 +5,10 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String url = "https://127.0.0.1/iot";
+        String url = "https://127.0.0.1:8443/savedata";
         Random r = new Random();
         for(int i=0;i<100;i++){
-            double lat = 36.800209 + r.nextDouble(0.005);
-            double lng = 127.074968 + r.nextDouble(0.005);
-            HttpSendData.send(url,"?lat="+lat+"&lng="+lng);
+            int num =
             try {
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
